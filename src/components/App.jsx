566a46a18d1db49e5b4c96474022f1b142b1e252
BunoOleg <86609721+BunoOleg@users.noreply.github.com>
2022-07-11 22:03:03 +0300

    345

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,79 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import styles from './App.module.css';
+import List from './List';
+import ContactForm from './Form';
+import Notification from './Notification';
+import Filter from './Filter';
+
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onSameName = data => {
+    return this.state.contacts.find(({ name }) => name === data.name);
+  };
+
+  onFormSubmit = data => {
+    if (this.state.contacts.length === 0) {
+      this.setState({ contacts: [data] });
+      return;
+    } else if (this.onSameName(data)) {
+      alert(`${data.name} is already in contacts.`);
+      return;
+    } else {
+      this.setState(prevState => ({
+        contacts: [data, ...prevState.contacts],
+      }));
+      return;
+    }
+  };
+
+  onInputHandler = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  filterReset = () => {
+    this.setState({ filter: '' });
+  };
+
+  onDeleteHandle = contactId => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(({ id }) => id !== contactId),
+    }));
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const filteredContacts = this.getFilteredContacts();
+
+    return (
+      <div className={styles.div}>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.onFormSubmit} />
+        <h2>Contacts</h2>
+        <Filter
+          onFilterInput={this.onInputHandler}
+          onBlur={this.filterReset}
+          value={this.state.filter}
+        />
+        {this.state.contacts.length > 0 ? (
+          <List contacts={filteredContacts} onDelete={this.onDeleteHandle} />
+        ) : (
+          <Notification />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
